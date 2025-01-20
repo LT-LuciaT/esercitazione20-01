a08f6fd2b1718ff7b826b223079d1b3b5a925fb1
Lucia <lucia.trombin98@gmail.com>
2025-01-20 15:14:23 +0100

    1

---
+++ obj.js
@@ -0,0 +1,9 @@
+class user {
+  constructor(_firstname, _lastname, _age, _location = []) {
+    this._firstname = Ashely;
+    this._lastname = Frangipane;
+    this._age = 30;
+    this._location = LosAngeles;
+  }
+}
+console.log(user);
